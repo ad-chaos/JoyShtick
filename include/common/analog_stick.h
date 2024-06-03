fe4688bcf3284632daa6e5542836f6c26223eefe
ad-chaos <santoshrajpurohit89@gmail.com>
2024-06-03 22:43:10 +0530

    remove unused values

--- include/common/analog_stick.h
+++ include/common/analog_stick.h
@@ -11,8 +11,6 @@
 
 #define YPIN 0
 #define XPIN 1
-#define RYPIN 2
-#define RXPIN 3
 #define JPINS 0xf
 
 typedef struct {
@@ -23,15 +21,11 @@ typedef struct {
 typedef struct {
   coffset_t x;
   coffset_t y;
-  coffset_t rx;
-  coffset_t ry;
 } callib_t;
 
 typedef struct {
   uint16_t x;
   uint16_t y;
-  uint16_t rx;
-  uint16_t ry;
 } analog_sticks_t;
 
 callib_t callibrate_zero(void);
@@ -40,6 +34,4 @@ hid_gamepad_report_t get_next_stick_report(const hid_gamepad_report_t, const ana
                                      const callib_t);
 uint16_t read_x(void);
 uint16_t read_y(void);
-uint16_t read_rx(void);
-uint16_t read_ry(void);
 analog_sticks_t read_all_axis(void);
--- src/analog_stick.c
+++ src/analog_stick.c
@@ -11,23 +11,11 @@ uint16_t INLINE read_y() {
   return adc_read();
 }
 
-uint16_t INLINE read_rx() {
-  adc_select_input(RXPIN);
-  return adc_read();
-}
-
-uint16_t INLINE read_ry() {
-  adc_select_input(RYPIN);
-  return adc_read();
-}
-
 analog_sticks_t INLINE read_all_axis() {
   adc_set_round_robin(JPINS);
   return (analog_sticks_t){
       .x = adc_read(),
       .y = adc_read(),
-      .rx = adc_read(),
-      .ry = adc_read(),
   };
 }
 
@@ -53,25 +41,20 @@ get_next_stick_report(const hid_gamepad_report_t current,
 
 callib_t callibrate_zero() {
   float zx = 0., zy = 0.;
-  float zrx = 0., zry = 0.;
 
   for (uint8_t i = 0; i < 64; i++) {
     analog_sticks_t sticks = read_all_axis();
 #define cumulative_average(of) z##of = (i * z##of + sticks.of) / (i + 1)
     cumulative_average(x);
     cumulative_average(y);
-    cumulative_average(rx);
-    cumulative_average(ry);
   }
 
 #define half_range(of) float hr##of = MAX(z##of, (1 << 12) - z##of);
   half_range(x);
   half_range(y);
-  half_range(rx);
-  half_range(ry);
 
-  return (callib_t){.x = {.zero = zx, .hrange = hrx},
-                    .y = {.zero = zy, .hrange = hry},
-                    .rx = {.zero = zrx, .hrange = hrrx},
-                    .ry = {.zero = zry, .hrange = hrry}};
+  return (callib_t){
+      .x = {.zero = zx, .hrange = hrx},
+      .y = {.zero = zy, .hrange = hry},
+  };
 }
